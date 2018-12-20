c96e7abd384d367cb1d1c2063ad70903b780e5c3
史琬琼 <qiong545026504@gmail.com>
2018-12-20 10:41:08 +0800

    首页文字滚动修复

--- js/textSlider.js
+++ js/textSlider.js
@@ -3,72 +3,48 @@
         //默认配置
         var defaults = {
             speed:40,  //滚动速度,值越大速度越慢
-            line:1     //滚动的行数
+            rowHeight:24 //每行的高度
         };
-        var opts = $.extend({}, defaults, options);
-        var $timer;
-        function marquee(obj, _speed){
-            var top = 0;
-            var margintop;
-            $timer = setInterval(function(){
-                top++;
-                margintop = 0-top;
-                obj.find("ul").animate({
-                    marginTop: margintop
-                },0,function(){
-                    var s = Math.abs(parseInt($(this).css("margin-top")));
-                    if(s >= 20){
-                        top = 0;
-                        $(this).css("margin-top", 0);   //确保每次都是从0开始，避免抖动
-                        $(this).find("li").slice(0, 1).appendTo($(this));
-                    }
-                });
-            }, _speed);
+
+        var opts = $.extend({}, defaults, options),intId = [];
+
+        function marquee(obj){
+            var step=obj.find('li').height();
+            obj.find("ul").animate({
+                marginTop: '-=1'
+            },0,function(){
+                var s = Math.abs(parseInt($(this).css("margin-top")));
+                if(s >= step){
+                    $(this).find("li").slice(0, 1).appendTo($(this));
+                    $(this).css("margin-top", 0);
+                }
+            });
         }
-        this.each(function(){
-            var speed = opts["speed"],line = opts["line"],_this = $(this);
-            var $ul =_this.find("ul");
-            if($ul.height() > _this.height()){
-                marquee(_this, speed);
-            }
 
-            //触摸开始
-            // _this.on('touchstart', function(ev){
-            //     ev.preventDefault();
-            //     clearInterval($timer);
-            // });
+        this.each(function(i){
+            var speed = opts["speed"],_this = $(this);
+            intId[i] = setInterval(function(){
+                if(_this.find("ul").height()<=_this.height()){
+                    clearInterval(intId[i]);
+                }else{
+                    marquee(_this);
+                }
+            }, speed);
 
-            //向上滑动
-            // _this.on('swipeup', function(ev){
-            //     ev.preventDefault();
-            //     clearInterval($timer);
-            //     if($ul.height() > _this.height()){
-            //         for(i=0;i<opts.line;i++){
-            //             $ul.find("li").first().appendTo($ul);
-            //         }
-            //         $ul.css("margin-top",0);
-            //     }
-            // });
+            _this.hover(function(){
+                clearInterval(intId[i]);
+            },function(){
+                intId[i] = setInterval(function(){
+                    if(_this.find("ul").height()<=_this.height()){
+                        clearInterval(intId[i]);
+                    }else{
+                        marquee(_this);
+                    }
+                }, speed);
+            });
 
-            //向下滑动
-            // _this.on('swipedown', function(ev){
-            //     ev.preventDefault();
-            //     clearInterval($timer);
-            //     if($ul.height() > _this.height()){
-            //         for(i=0;i<opts.line;i++){
-            //             $ul.find("li").first().before($ul.find("li").last());
-            //         }
-            //         $ul.css("margin-top",0);
-            //     }
-            // });
-
-            //触摸结束
-            // _this.on('touchend',function(ev){
-            //     ev.preventDefault();
-            //     if($ul.height() > _this.height()){
-            //         marquee(_this, speed);
-            //     }
-            // });
         });
+
     }
+
 })(jQuery);
